75df2f7731adea20c722941f6acf2a50e40ecc13
BlueSharkSystems <programming@bluesharksystems.com>
2017-01-24 22:19:14 +0000

    modificacion de variables
    Abreviado el nombre de las variables, para hacerlas con nombres mas cortos

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@ public class Main {
 //Declaracion de una clase
 	// dato tipo entero
 	public static void main(String[] argumentos){
-		int numero = 100, numero2 = 20, resultado;
-    		resultado = numero + numero2;
-    		System.out.println(resultado);
+		int num = 100, num2 = 20, resul;
+    		resul = num + num2;
+    		System.out.println(resul);
 	   
     		}
 	}
