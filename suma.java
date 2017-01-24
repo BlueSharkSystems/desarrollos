51e01aed0e2e9abab80de2c694d680e85a8336a5
BlueSharkSystems <programming@bluesharksystems.com>
2017-01-24 22:09:32 +0000

    Crear el nuevo archivo suma.java

---
+++ suma.java
@@ -0,0 +1,13 @@
+public class Main {
+//Declaracion de una clase
+	// dato tipo entero
+	public static void main(String[] argumentos){
+		int numero = 100;
+		int numero2 = 20;
+    int resultado;
+    
+    resultado = numero + numero2;
+    System.out.println(resultado);
+	   
+    }
+	}
