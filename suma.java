15e01216a6867617da9e5386ac67b4a3f98aed6f
BlueSharkSystems <programming@bluesharksystems.com>
2017-01-24 22:15:34 +0000

    optimizado de la declaración de variables
    Puesto en solo una linea de programacion la declaración de variables

--- suma.java
+++ suma.java
@@ -2,12 +2,9 @@ public class Main {
 //Declaracion de una clase
 	// dato tipo entero
 	public static void main(String[] argumentos){
-		int numero = 100;
-		int numero2 = 20;
-    int resultado;
-    
-    resultado = numero + numero2;
-    System.out.println(resultado);
+		int numero = 100, numero2 = 20, resultado;
+    		resultado = numero + numero2;
+    		System.out.println(resultado);
 	   
-    }
+    		}
 	}
