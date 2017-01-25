2338ed245ab8a66bf328948ce5c5cb8647c6ffff
BlueSharkSystems <programming@bluesharksystems.com>
2017-01-25 17:57:38 +0000

    cambio de valor de las variables

--- suma.java
+++ suma.java
@@ -2,7 +2,7 @@ public class Main {
 //Declaracion de una clase
 	// dato tipo entero
 	public static void main(String[] argumentos){
-		int num = 100, num2 = 20, resul;
+		int num = 40, num2 = 15, resul;
     		resul = num + num2;
     		System.out.println(resul);
 	   
